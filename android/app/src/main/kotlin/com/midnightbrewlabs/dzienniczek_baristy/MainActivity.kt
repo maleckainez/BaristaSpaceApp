68e7caf3152024de554a62343536d7e1901fb2c5
Inez Małecka <maleckainez@gmail.com>
2024-10-15 20:10:40 +0200

    pushing already existing project

---
+++ android/app/src/main/kotlin/com/midnightbrewlabs/dzienniczek_baristy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.midnightbrewlabs.dzienniczek_baristy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:dzienniczek_baristy/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Barista\'s Notes',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+        scaffoldBackgroundColor: Colors.white,
+    ),
+      home: LoginScreen(),
+    );
+  }
+}
