6f5d5bf613613a5fb099648669c636205cf310c0
Inez Małecka <maleckainez@gmail.com>
2024-12-24 01:48:49 +0100

    Entry version of wikipedia, needs remodeling

---
+++ lib/screens/main_screens/wiki_element.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class WikiElement extends StatelessWidget {
+
+  final String title;
+  final String subtitle;
+  final String description;
+
+  WikiElement({
+    required this.title,
+    required this.subtitle,
+    required this.description,
+});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(40),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 7,
+            offset: Offset(2, 2), // Przesunięcie cienia
+          ),
+        ],
+        color: Colors.white,
+      ),
+      width: 350,
+      height: 175,
+      child: Padding(padding: EdgeInsets.all(30),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(title,
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w900,
+            fontFamily: 'RobotoSlab',
+
+          ),),
+          SizedBox(height: 5,),
+          Text(subtitle,
+          style: TextStyle(
+            fontFamily: 'RobotoSlab',
+            fontSize: 10,
+          ),),
+          SizedBox(height: 15,),
+          Text(description,
+          style: TextStyle(
+            fontFamily: 'RobotoSlab',
+            fontSize: 10,
+          ),)
+        ],
+      )),
+    );
+  }
+}
--- lib/screens/main_screens/wiki_screen.dart
+++ lib/screens/main_screens/wiki_screen.dart
@@ -1,45 +1,95 @@
+import 'package:dzienniczek_baristy/screens/main_screens/wiki_element.dart';
 import 'package:flutter/material.dart';
 
-class WikiScreen extends StatefulWidget{
-  @override 
+class WikiScreen extends StatefulWidget {
+  @override
   _WikiScreenState createState() => _WikiScreenState();
 }
-class _WikiScreenState extends State<WikiScreen>{
+
+class _WikiScreenState extends State<WikiScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(221590837),
-        title: Row(
-          children: [
-            Text("Wiki for your coffee",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.grey,
-            ),),
-            SizedBox(width: 10,),
-            Icon(Icons.favorite_border_rounded,
-            color: Colors.grey,),
-          ]
-      ),),
-      body: SafeArea(
-        child: Center(
-          child: Padding(
-              padding: const EdgeInsets.all(20.0),
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  
-                  
-                ],
+    return DefaultTabController(
+      length: 4, // Liczba zakładek
+      child: Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Color(Colors.grey.value)),
+          backgroundColor: Color(0x0d353535),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Expand your knowledge",
+                style: TextStyle(
+                  fontFamily: 'RobotoSlab',
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
               ),
+              SizedBox(width: 10),
+              Icon(
+                Icons.coffee_outlined,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                print("object");
+              },
+              icon: Icon(Icons.search),
+              tooltip: "Search through wiki",
             ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.category_outlined),
+              tooltip: "Change category",
+            ),
+          ],
+          bottom: TabBar(
+            tabAlignment: TabAlignment.start,
+            isScrollable: true,
+            indicatorColor: Color(0xFF6F4E37),
+            labelColor: Color(0xFF6F4E37),
+            unselectedLabelColor: Color(Colors.grey.value),
+            labelStyle: TextStyle(
+              color: Color(0xFF6F5E37),
+              fontFamily: 'RobotoSlab',
+              fontWeight: FontWeight.w100,
+            ),
+            tabs: [
+              Tab(text: 'Category 1'),
+              Tab(text: 'Category 2'),
+              Tab(text: 'Category 3'),
+              Tab(text: 'Category 4'),
+            ],
           ),
         ),
-      ),
+        body:  TabBarView(
+          children: [
+            SingleChildScrollView(
+              padding: EdgeInsets.zero,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(height: 20),
+                WikiElement(title: "Water", subtitle: "Learn how important water can be.", description: "Lorem Ipsum",),
+                SizedBox(height: 20),
+                WikiElement(title: "Water", subtitle: "Learn how important water can be.", description: "Lorem Ipsum",),
+                SizedBox(height: 20),
+                WikiElement(title: "Water", subtitle: "Learn how important water can be.", description: "Lorem Ipsum",),
+                SizedBox(height: 20),
+                WikiElement(title: "Water", subtitle: "Learn how important water can be.", description: "Lorem Ipsum",),
+                SizedBox(height: 20),
+              ],
+            ),),
+            Center(child: Text('Content for Category 2')),
+            Center(child: Text('Content for Category 3')),
+            Center(child: Text('Content for Category 4')),
+          ],
+        )),
     );
   }
-  
 }
